380d733dd7e27a66647aa8e0bf09027b20700828
Kamil <kaz4486@gmail.com>
2022-10-18 17:40:00 +0200

    Change server.js

--- server.js
+++ server.js
@@ -40,6 +40,7 @@ if (process.env.NODE_ENV !== 'production') {
 if (process.env.NODE_ENV === 'production') {
   app.use(
     cors({
+      origin: ['https://lit-wave-35985.herokuapp.com'],
       credentials: true,
     })
   );
