8318e2be3bbb8c332bc891996da325bee49bb216
Kamil <kaz4486@gmail.com>
2022-10-19 22:16:28 +0200

    Change server v11

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ const db = mongoose.connection;
 
 app.use(
   cors({
-    origin: ['http://localhost:3000', 'http://localhost:8000'],
+    // origin: ['http://localhost:3000', 'http://localhost:8000'],
     credentials: true,
   })
 );
