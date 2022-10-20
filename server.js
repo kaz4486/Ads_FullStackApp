6b8bc2d67dac571ad4950e95e9be4afec207ede8
Kamil <kaz4486@gmail.com>
2022-10-20 10:32:33 +0200

    Change server v12

--- server.js
+++ server.js
@@ -37,7 +37,11 @@ const db = mongoose.connection;
 
 app.use(
   cors({
-    origin: ['http://localhost:3000', 'http://localhost:8000'],
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:8000',
+      'https://lit-wave-35985.herokuapp.com/',
+    ],
     credentials: true,
   })
 );
