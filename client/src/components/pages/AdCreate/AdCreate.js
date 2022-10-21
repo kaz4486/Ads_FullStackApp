552dbc9d139175b0b35ffda4340ae8efce73b721
Kamil <kaz4486@gmail.com>
2022-10-21 12:06:47 +0200

    Fix loading in AdEdit, Fix loading in AdCreate

--- client/src/components/pages/AdCreate/AdCreate.js
+++ client/src/components/pages/AdCreate/AdCreate.js
@@ -1,11 +1,12 @@
 // import { Col, Container, Row, Button } from 'react-bootstrap';
 import { useState } from 'react';
 import { Alert } from 'react-bootstrap';
+import { Progress } from 'reactstrap';
 import { AUTH_URL } from '../../../configs/config';
 import CreateAdForm from '../../features/CreateAdForm/CreateAdForm';
 
 const AdCreate = () => {
-  const [user, setUser] = useState(false);
+  const [user, setUser] = useState('loading'); //loading, user, noUser, serverError
 
   const options = {
     method: 'GET',
@@ -15,20 +16,30 @@ const AdCreate = () => {
   fetch(`${AUTH_URL}/user`, options)
     .then((res) => {
       if (res.status === 200) {
-        setUser(true);
+        setUser('user');
+      } else {
+        setUser('noUser');
       }
     })
     .catch((err) => {
       setUser('serverError');
     });
 
-  if (!user)
+  if (user === 'noUser')
     return (
       <Alert color='warning' className='mt-2'>
-        'You are not authorized, First Log In'
+        You are not authorized, First Log In
       </Alert>
     );
-  if (user) {
+  if (user === 'serverError')
+    return (
+      <Alert color='danger' className='mt-2'>
+        Something went wrong...
+      </Alert>
+    );
+  if (user === 'loading')
+    return <Progress animated color='primary' value={50} />;
+  if (user === 'user') {
     return (
       <div>
         <h1>Ad create</h1>
--- client/src/components/pages/AdEdit/AdEdit.js
+++ client/src/components/pages/AdEdit/AdEdit.js
@@ -2,9 +2,10 @@ import { Alert } from 'react-bootstrap';
 import EditAdForm from '../../features/EditAdForm/EditAdForm';
 import { useState } from 'react';
 import { AUTH_URL } from '../../../configs/config';
+import { Progress } from 'reactstrap';
 
 const AdEdit = () => {
-  const [user, setUser] = useState(false);
+  const [user, setUser] = useState('loading'); //loading, user, noUser, serverError
 
   const options = {
     method: 'GET',
@@ -14,20 +15,36 @@ const AdEdit = () => {
   fetch(`${AUTH_URL}/user`, options)
     .then((res) => {
       if (res.status === 200) {
-        setUser(true);
+        setUser('user');
+      } else {
+        setUser('noUser');
       }
     })
     .catch((err) => {
       setUser('serverError');
     });
 
+  if (user === 'noUser')
+    return (
+      <Alert color='warning' className='mt-2'>
+        You are not authorized, First Log In
+      </Alert>
+    );
+  if (user === 'serverError')
+    return (
+      <Alert color='danger' className='mt-2'>
+        Something went wrong...
+      </Alert>
+    );
+  if (user === 'loading')
+    return <Progress animated color='primary' value={50} />;
   if (!user)
     return (
       <Alert color='warning' className='mt-2'>
         'You are not authorized, First Log In'
       </Alert>
     );
-  if (user) {
+  if (user === 'user') {
     return (
       <div>
         <h1>Edit Ad</h1>
