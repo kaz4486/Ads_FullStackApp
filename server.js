6218ff9e48c6e7d8ff192475a9dcfe32a755eada
Kamil <kaz4486@gmail.com>
2023-03-26 20:03:43 +0200

    Rebuild

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-import express, { urlencoded, json } from 'express';
-import cors from 'cors';
-import { join } from 'path';
-import { connect, connection } from 'mongoose';
-import session from 'express-session';
-import { create } from 'connect-mongo';
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 require('dotenv').config();
 
 const app = express();
@@ -17,11 +17,11 @@ const SECRET = process.env.SECRET;
 
 connectionString = `mongodb+srv://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@cluster0.telw8lc.mongodb.net/Ads_FullStackAppDB?retryWrites=true&w=majority`;
 
-connect(connectionString, {
+mongoose.connect(connectionString, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-const db = connection;
+const db = mongoose.connection;
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
@@ -41,8 +41,8 @@ app.use(
   })
 );
 
-app.use(urlencoded({ extended: false }));
-app.use(json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.set('trust proxy', 1);
 app.use(
@@ -50,7 +50,7 @@ app.use(
     secret: SECRET,
     resave: false,
     saveUninitialized: true,
-    store: create({
+    store: MongoStore.create({
       mongoUrl: connectionString,
       collection: 'sessions',
     }),
@@ -60,19 +60,19 @@ app.use(
   })
 );
 
-import adsRoutes from './routes/ads.routes';
-import authRoutes from './routes/auth.routes';
+const adsRoutes = require('./routes/ads.routes');
+const authRoutes = require('./routes/auth.routes');
 
 app.use('/api', adsRoutes);
 // app.use('/api', userRoutes);
 app.use('/auth', authRoutes);
 // może być też api/auth
 
-app.use(static(join(__dirname, '/client/build')));
-app.use(static(join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, '/client/build')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('*', (req, res) => {
-  res.sendFile(join(__dirname, '/client/build/index.html'));
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
 });
 
 db.once('open', () => {
